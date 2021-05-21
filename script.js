0c3cefc7f7e668dc5113a503bb904d2cc5920716
abhinav-003 <74131197+abhinav-003@users.noreply.github.com>
2021-05-21 12:48:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+function ChangeColor()  {
+   var btn1 = document.getElementById('btn1')
+   btn1.style.backgroundColor='blue'; 
+   btn1.style.color='white'
+}
+function ChangeColor2()  {
+   var btn2 = document.getElementById('btn2').style.backgroundColor='blue'; 
+    document.getElementById('btn2').style.color='white'
+}
+ 
+function ChangeColor3()  {
+    var btn3 = document.getElementById('btn3').style.backgroundColor='blue'; 
+    document.getElementById('btn3').style.color='white'
+}
+function ChangeColor4()  {
+    var btn4 = document.getElementById('btn4').style.backgroundColor='blue'; 
+    document.getElementById('btn4').style.color='white'
+}
+function ChangeColor5()  {
+    var btn5 = document.getElementById('btn5').style.backgroundColor='blue'; 
+    document.getElementById('btn5').style.color='white'
+}
+function ChangeColor6()  {
+    var btn6 = document.getElementById('btn6').style.backgroundColor='blue'; 
+    document.getElementById('btn6').style.color='white'
+}
+function ChangeColor7()  {
+    var btn7 = document.getElementById('btn7').style.backgroundColor='blue'; 
+    document.getElementById('btn7').style.color='white'
+}
+function ChangeColor8()  {
+    var btn8 = document.getElementById('btn8').style.backgroundColor='blue'; 
+    document.getElementById('btn8').style.color='white'
+}
+function ChangeColor9()  {
+    var btn9 =  document.getElementById('btn9').style.backgroundColor='blue'; 
+    document.getElementById('btn9').style.color='white'
+}
+function ChangeColor10()  {
+    var btn10 = document.getElementById('btn10').style.backgroundColor='blue'; 
+    document.getElementById('btn10').style.color='white'
+}
+ 
+
+     
+ 
+ 
